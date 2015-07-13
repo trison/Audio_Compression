5773e2fcbb5a33a358962e1cb076955d94471069
Trison Nguyen <trisonbob@hotmail.com>
2015-07-13 11:45:32 -0700

    Generate array of samples, print them

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
-
+#include <math.h>
 #include <stdio.h>
  
 int main()
 {
+	int i=0;
+	unsigned int samples[65537];
+	int result;
+	
+	//generate samples
+	for(int i =0; i<=65536; i++){
+		samples[i] = i;
+	}
 
-
-
-
-
-
-
+	//print samples
+	int j;
+	for(j=65536; j!=0; j--){
+		result = log(samples[j])/log(2);
+		printf("result: %d\n", result);
+	}
 
 
 
 	
-  printf("Hello world\n");
+
+
+
   return 0;
 }
